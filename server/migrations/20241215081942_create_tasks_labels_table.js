d28a9f24008c931f6e291ebc66aa86c334e45889
kozak <kozak@192.168.1.19>
2024-12-17 20:35:38 +0300

    Fixed linter errors

--- server/migrations/20241215081942_create_tasks_labels_table.js
+++ server/migrations/20241215081942_create_tasks_labels_table.js
@@ -1,11 +1,11 @@
 export const up = (knex) => (
-    knex.schema.createTable('tasks_labels', (table) => {
-        table.increments('id').primary();
-        table.integer('task_id').references('tasks.id').notNullable();
-        table.integer('label_id').references('labels.id').notNullable();
-        table.timestamp('created_at').defaultTo(knex.fn.now());
-        table.timestamp('updated_at').defaultTo(knex.fn.now());
-    })
+  knex.schema.createTable('tasks_labels', (table) => {
+    table.increments('id').primary();
+    table.integer('task_id').references('tasks.id').notNullable();
+    table.integer('label_id').references('labels.id').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  })
 );
 
 export const down = (knex) => knex.schema.dropTable('tasks_labels');
